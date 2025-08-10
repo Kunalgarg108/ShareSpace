20c44f18e560350ccf5444783d334c36fa5443bf
KUNAL KUMAR  GARG <162574220+Kunalgarg108@users.noreply.github.com>
2025-08-10 22:50:38 +0530

    Update socket.js

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin:process.env.FRONTEND_URL,
+        origin: "https://sharespace-ruddy.vercel.app",
         methods:['GET','POST']
     }
 })
