865e7ccd48cdcb44a5beb59e5c4c9b3f1abd7649
KUNAL KUMAR  GARG <162574220+Kunalgarg108@users.noreply.github.com>
2025-08-10 23:09:26 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -67,7 +67,7 @@ export const login = async (req, res) => {
       bookmarks: user.bookmarks,
       likedPosts: user.likedPosts,
     }
-    return res.cookie('token', token, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 , secure : true}).status(200).json({ success: true, message: "Login successful", user });
+    return res.cookie('token', token, { httpOnly: true, sameSite: 'Strict', maxAge: 24 * 60 * 60 * 1000 , secure : true}).status(200).json({ success: true, message: "Login successful", user });
   } catch (error) {
     res.status(500).json({ success: false, message: "Server error", error: error.message });
   }
@@ -75,7 +75,7 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie('token', { httpOnly: true, sameSite: 'None', maxAge: 0 , secure : true });
+    res.clearCookie('token', { httpOnly: true, sameSite: 'Strict', maxAge: 0 , secure : true });
     return res.status(200).json({ success: true, message: "Logout successful" });
   } catch (error) {
     res.status(500).json({ success: false, message: "Server error", error: error.message });
